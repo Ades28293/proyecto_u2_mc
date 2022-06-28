f25a00b34b8a569acb60df3ffc1e725f71c934a8
Michael Cen <cenyungamichael@gmail.com>
2022-06-27 20:24:53 -0500

    taller15

--- src/main/java/com/uce/edu/demo/ProyectoU2McApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2McApplication.java
@@ -1,13 +1,22 @@
 package com.uce.edu.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import com.uce.edu.demo.service.IPersonaJdbcService;
+import com.uce.edu.demo.to.Persona;
+
+
 @SpringBootApplication
 public class ProyectoU2McApplication implements CommandLineRunner{
 	private static final Logger LOGGER = LoggerFactory.getLogger(ProyectoU2McApplication.class);
+	
+	@Autowired
+	private IPersonaJdbcService iPersonaJdbcService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2McApplication.class, args);
@@ -15,10 +24,31 @@ public class ProyectoU2McApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		//System.out.println("Hola mundo");
 		
-		LOGGER.info("Hola Mundo");
+		Persona p1=new Persona();
+		p1.setId(2);
+		p1.setNombre("Juan");
+		p1.setApellido("Perez");
+		
+		//this.iPersonaJdbcService.guardar(p1);
+		
+		
+		
+		Persona p2=new Persona();
+		p2.setId(2);
+		p2.setNombre("Pepito");
+		p2.setApellido("Velez");
+	
+		//Actualizar
+		//	this.iPersonaJdbcService.actualizar(p2);
+		
+		//elimianr
+		//this.iPersonaJdbcService.eliminar(1);
+		
+		
+		//buscar
+		this.iPersonaJdbcService.buscar(2);
+		
 		
 		
 	}
--- src/main/java/com/uce/edu/demo/repository/PersonaJdbcRepositoryImpl.java
+++ src/main/java/com/uce/edu/demo/repository/PersonaJdbcRepositoryImpl.java
@@ -1,5 +1,50 @@
 package com.uce.edu.demo.repository;
 
-public class PersonaJdbcRepositoryImpl {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+import com.uce.edu.demo.to.Persona;
+
+@Repository
+public class PersonaJdbcRepositoryImpl implements IPersonaJdbcRepository {
+	private static final Logger LOGGER = LoggerFactory.getLogger(PersonaJdbcRepositoryImpl.class);
+
+	// Inyeccion jdbc
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	
+	@Override
+	public Persona buscarId(int id) {
+		// TODO Auto-generated method stub
+		LOGGER.info("Se busco el usuario:"+ id);
+		return this.jdbcTemplate.queryForObject("select * from persona WHERE id=?", new Object[] { id },
+				new BeanPropertyRowMapper<Persona>(Persona.class));
+	}
+	@Override
+	public void insertar(Persona persona) {
+		// TODO Auto-generated method stub
+		this.jdbcTemplate.update("insert into persona (id,nombre,apellido) values (?,?,?)",
+				new Object[] { persona.getId(), persona.getNombre(), persona.getApellido() });
+
+	}
+
+	@Override
+	public void actualizar(Persona persona) {
+		// TODO Auto-generated method stub
+		this.jdbcTemplate.update("update persona set nombre=?,apellido=? where id=?",
+				new Object[] { persona.getNombre(), persona.getApellido(), persona.getId() });
+
+	}
+
+	@Override
+	public void eliminar(int id) {
+		// TODO Auto-generated method stub
+		LOGGER.info("Se elimine el usuario:"+ id);
+		this.jdbcTemplate.update("delete from persona where id=?", new Object[] { id });
+	}
 }

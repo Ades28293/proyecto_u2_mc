da3b46d4034da465ce5847f5af4e3b6ec218ea3c
Michael Cen <cenyungamichael@gmail.com>
2022-07-25 20:14:33 -0500

    taller26 JPA ManyToMany

--- src/main/java/com/uce/edu/demo/ProyectoU2McApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2McApplication.java
@@ -1,5 +1,9 @@
 package com.uce.edu.demo;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -9,14 +13,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.uce.edu.demo.correcion.service.IMatriculaGestorService;
 import com.uce.edu.demo.correcion.service.IPropietarioService;
 import com.uce.edu.demo.correcion.service.IVehiculoService;
-import com.uce.edu.demo.repository.modelo.onetomany.Habitacion;
-import com.uce.edu.demo.repository.modelo.onetomany.Hotel;
+import com.uce.edu.demo.repository.modelo.manytomany.Autor;
+import com.uce.edu.demo.repository.modelo.manytomany.Libro;
+import com.uce.edu.demo.service.IAutorService;
 import com.uce.edu.demo.service.ICiudadanoJpaService;
 import com.uce.edu.demo.service.IHabitacionService;
 import com.uce.edu.demo.service.IHotelService;
+import com.uce.edu.demo.service.ILibroService;
 import com.uce.edu.demo.service.IPersonaJdbcService;
 import com.uce.edu.demo.service.IPersonaJpaService;
-import com.uce.edu.demo.tarea.repository.modelo.Medico;
 import com.uce.edu.demo.tarea.service.ICiudadanoService;
 import com.uce.edu.demo.tarea.service.IEstudianteJpaService;
 import com.uce.edu.demo.tarea.service.IMedicoService;
@@ -55,13 +60,19 @@ public class ProyectoU2McApplication implements CommandLineRunner {
 
 	@Autowired
 	private IHabitacionService habitacionService;
-	
+
 	@Autowired
 	private IMedicoService iMedicoService;
-	
+
 	@Autowired
 	private IRecetaMedicaService iRecetaMedicaService;
-	
+
+	@Autowired
+	private ILibroService iLibroService;
+
+	@Autowired
+	private IAutorService autorService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2McApplication.class, args);
 	}
@@ -69,30 +80,34 @@ public class ProyectoU2McApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		//Insertar
-		Medico medi=new Medico();
-		medi.setNombre("Estefania");
-		medi.setApellido("Lopez");
-		
-		this.iMedicoService.insertar(medi);
-		
-		//Actualizar
-		Medico medi2=new Medico();
-		medi2.setId(1);
-		medi2.setNombre("Michael");
-		medi2.setApellido("Cen");
-		this.iMedicoService.actualizar(medi2);
-		
-		//Buscar
-		LOGGER.info("Medico: "+this.iMedicoService.buscar(2));
-		
-		//Elimina
-		this.iMedicoService.eliminar(2);
-		
-		
-		
-		
-		
+		/*
+		 * //Insertar Medico medi=new Medico(); medi.setNombre("Estefania");
+		 * medi.setApellido("Lopez");
+		 * 
+		 * this.iMedicoService.insertar(medi);
+		 * 
+		 * //Actualizar Medico medi2=new Medico(); medi2.setId(1);
+		 * medi2.setNombre("Michael"); medi2.setApellido("Cen");
+		 * this.iMedicoService.actualizar(medi2);
+		 * 
+		 * //Buscar LOGGER.info("Medico: "+this.iMedicoService.buscar(2));
+		 * 
+		 * //Elimina this.iMedicoService.eliminar(2);
+		 */
+
+		Autor autor1 = new Autor();
+		autor1.setNombre("Juan Perez");
+
+		Set<Autor> autores = new HashSet<>();
+		autores.add(autor1);
+
+		Libro libr = new Libro();
+		libr.setTitulo("JPA");
+		libr.setCantidadPaginas(250);
+
+		libr.setAutores(autores);
+
+		this.iLibroService.insertar(libr);
 
 	}
 
---
+++ src/main/java/com/uce/edu/demo/repository/AutorRepositoryImpl.java
@@ -0,0 +1,24 @@
+package com.uce.edu.demo.repository;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.uce.edu.demo.repository.modelo.manytomany.Autor;
+
+@Repository
+@Transactional
+public class AutorRepositoryImpl implements IAutorRepository{
+
+	@Autowired
+	private EntityManager entityManager;
+	
+	@Override
+	public void insertar(Autor autor) {
+		// TODO Auto-generated method stub
+		this.entityManager.persist(autor);
+	}
+
+}
